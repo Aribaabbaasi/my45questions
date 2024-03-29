35cfb434432d93943003fe7c5cf4f8edb0f89171
Ariba Abbai <165388999+Aribaabbaasi@users.noreply.github.com>
2024-03-29 21:56:05 +0500

    Add files via upload

---
+++ more_conditional_tests.js
@@ -0,0 +1,9 @@
+"use strict";
+// Equality and Inequality Test
+console.log("Equality test with strings: ", "Apple" === "Apple");
+// Equality and Inequality Test
+console.log("Inequality test with strings: ", "Apple" != "orange");
+// Tests using the lower case function
+console.log("Lower Case function test: ", "Hello".toLowerCase() === "hello");
+// Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
+console.log("Equality test with number:, 26 === 26");
