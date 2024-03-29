384e41302d65768be001769ca0dfd464e05b5b91
Ariba Abbai <165388999+Aribaabbaasi@users.noreply.github.com>
2024-03-29 21:51:36 +0500

    Add files via upload

--- Guest_List.js
+++ Guest_List.js
@@ -9,21 +9,24 @@ Guest_List[0] = new_Guest;
 // for(let i=0; i<Guest_List.length; i++){
 // console.log('Dear Mr. ' + Guest_List[i] +',\n\nIt is our pleasure to innvite you in our party.\n\nThank You!\n\n')   
 // }
-console.log(`Mr. ${absent_Guest} is not coming to the party.`);
-console.log('Good News! we find big table so we are inviting 3 more guests.');
+// console.log(`Mr. ${absent_Guest} is not coming to the party.`);
+// console.log('Good News! we find big table so we are inviting 3 more guests.')
 Guest_List.unshift('Sir Zia Khan');
 Guest_List.splice(2, 0, 'Maryam Nawaz');
 Guest_List.push('Bilawal Bhutto Zardari');
-for (let i = 0; i < Guest_List.length; i++) {
-    console.log('Dear Mr. ' + Guest_List[i] + ',\n\nIt is our pleasure to innvite you in our party.\n\nThank You!\n\n');
-}
-console.log('\nSorry we can not arrange big table, Only two peoples will be invited.');
+// for(let i=0; i<Guest_List.length; i++){
+//     console.log('Dear Mr. ' + Guest_List[i] +',\n\nIt is our pleasure to innvite you in our party.\n\nThank You!\n\n')   
+// }
+// console.log('\nSorry we can not arrange big table, Only two peoples will be invited.');
 while (Guest_List.length > 2) {
     let remove_Guest = Guest_List.pop();
     console.log(`Sorry Mr. ${remove_Guest},You are not invited for Dinner.`);
 }
-for (let i = 0; i < Guest_List.length; i++) {
-    console.log('Dear Mr. ' + Guest_List[i] + ',\n\nYou are still invited.\n\nThank You!\n\n');
-}
+// for(let i=0; i<Guest_List.length; i++){
+//     console.log('Dear Mr. ' + Guest_List[i] +',\n\nYou are still invited.\n\nThank You!\n\n')   
+// }
 Guest_List.splice(0, 2);
 console.log(Guest_List);
+// Excercise 19
+//print a message indicating the number of people you are inviting to dinner.
+console.log(`Total number of Guest Are: ${Guest_List.length}`);
