d77d9d9471b770530642fdbc72966ce8e6083d19
Ariba Abbai <165388999+Aribaabbaasi@users.noreply.github.com>
2024-03-29 22:08:33 +0500

    Add files via upload

---
+++ Sandwish.ts
@@ -0,0 +1,15 @@
+// Sandwiches: Write a function that accepts a array of items a person wants on a sandwich. The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that is being ordered. Call the function three times, using a different number of arguments each time.
+
+function makeSandwish (item: string[]) {
+    console.log('\nMaking your sandwish with:');
+
+    item.forEach(element => console.log("- " + element));
+
+    console.log('Enjoy your sandwish |\n');
+}
+
+makeSandwish(['Ham','cheese','lettuce']);
+
+makeSandwish(['Turkey','Bacon']);
+
+makeSandwish(['Peanut butter','Jelly']);
