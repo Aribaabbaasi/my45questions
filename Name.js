d7d85d836bacdcbe14b39945c33d6eec3adc6a07
Ariba Abbai <165388999+Aribaabbaasi@users.noreply.github.com>
2024-03-29 21:45:17 +0500

    Add files via upload

---
+++ Name.js
@@ -0,0 +1,5 @@
+"use strict";
+let names = ['Ariba Abbasi', 'Afza Abbasi', 'Bisma Abbasi', 'Manal Abbasi'];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ Name.ts
@@ -0,0 +1,7 @@
+
+
+let names :string[] = ['Ariba Abbasi','Afza Abbasi','Bisma Abbasi','Manal Abbasi'] ;
+
+for(let i=0; i<names.length; i++){
+    console.log(names[i]) ;
+}
