dbacf4c04b145dae0d9075184884fc45d343a77a
Ariba Abbai <165388999+Aribaabbaasi@users.noreply.github.com>
2024-03-29 21:48:55 +0500

    Add files via upload

--- Guest_List.ts
+++ Guest_List.ts
@@ -1,9 +1,23 @@
 
 
-let Guest_List :string[] = ['Imran khan','Nawaz Sharif','Asif Ali Zardari'];
+let Guest_List :string[] = ['Imran Khan','Nawaz Sharif','Asif Ali Zardari'];
 
-for(let i=0; i< Guest_List.length; i++){
+for(let i=0; i<Guest_List.length; i++){
 
-    console.log('Dear Mr. ' + Guest_List[i] +',\n\nIt is our pleasure to innvite you in our party.\n\nThank You!')
+    console.log('Dear Mr. ' + Guest_List[i] +',\n\nIt is our pleasure to innvite you in our party.\n\nThank You!\n\n')
     
 }
+
+let absent_Guest :string = 'Imran Khan' ;
+
+let new_Guest :string = 'Kamran Khan Tessori' ;
+
+Guest_List[0] = new_Guest ;
+
+for(let i=0; i<Guest_List.length; i++){
+
+    console.log('Dear Mr. ' + Guest_List[i] +',\n\nIt is our pleasure to innvite you in our party.\n\nThank You!\n\n')
+    
+}
+
+console.log(`Mr. ${absent_Guest} is not coming to the party.`);
