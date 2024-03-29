068068f40de46bca533bade0982a1aa030a2f7c9
Ariba Abbai <165388999+Aribaabbaasi@users.noreply.github.com>
2024-03-29 21:54:53 +0500

    Add files via upload

---
+++ International_Error.ts
@@ -0,0 +1,14 @@
+//  If you haven’t received an array index error in one of your programs yet, try to make one happen.
+
+
+// Creating array
+let newArray :string[] = ['Ariba Abbasi','Azan Abbasi','Afza Abbasi','Manal Abbasi'];
+
+// Accessing array by index
+console.log(newArray[5]);
+
+
+// Make sure you correct the error before closing the program.
+
+// Correct the eeror
+console.log(newArray);
