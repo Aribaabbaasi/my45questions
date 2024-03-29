60a9bcd0ab276404f2b114936a6a1c875908ca95
Ariba Abbai <165388999+Aribaabbaasi@users.noreply.github.com>
2024-03-29 21:55:20 +0500

    Add files via upload

---
+++ conditional_tests.js
@@ -0,0 +1,34 @@
+"use strict";
+// Write a series of conditional tests.
+// Creating car variable
+let car = 'subaru';
+// Test NO 1
+console.log("Is car === 'subaru'? I predict True.");
+console.log(car === 'subaru');
+//  Test NO 2
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+// Test NO 3
+console.log("Is car != 'Honda'? I predict True.");
+console.log(car != 'Honda');
+// Test NO 4
+console.log("Is car !== 'ford'? I predict True.");
+console.log(car !== 'ford');
+// Test NO 5
+console.log("Is car.Upper case == 'SUBARU'? I predict True.");
+console.log(car.toUpperCase() == 'SUBARU');
+// Test NO 6
+console.log("Is car == 'SUBARU'? I predict False.");
+console.log(car == 'SUBARU');
+// Test NO 7
+console.log("Is car === 'SUBARU'? I predict False.");
+console.log(car === 'SUBARU');
+//  Test NO 8
+console.log("Is car === 'Train'? I predict False.");
+console.log(car === 'Train');
+// Test NO 9
+console.log("Is car == 'Bus'? I predict False.");
+console.log(car == 'Bus');
+// Test NO 10
+console.log("Is car == 'Cycle'? I predict False.");
+console.log(car == 'Cycle');
