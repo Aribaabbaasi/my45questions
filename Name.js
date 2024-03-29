5c0f94cb69f72f2246fd36a85b59f73eae3de06c
Ariba Abbai <165388999+Aribaabbaasi@users.noreply.github.com>
2024-03-29 21:46:18 +0500

    Add files via upload

--- Name.js
+++ Name.js
@@ -1,5 +1,6 @@
 "use strict";
 let names = ['Ariba Abbasi', 'Afza Abbasi', 'Bisma Abbasi', 'Manal Abbasi'];
+const message = 'Welcome to Typescript class: ';
 for (let i = 0; i < names.length; i++) {
     console.log(names[i]);
 }
--- Name.ts
+++ Name.ts
@@ -2,6 +2,10 @@
 
 let names :string[] = ['Ariba Abbasi','Afza Abbasi','Bisma Abbasi','Manal Abbasi'] ;
 
+const message :string = 'Welcome to Typescript class:'; 
+
 for(let i=0; i<names.length; i++){
+
     console.log(names[i]) ;
+
 }
