3e46e7b2dfc2bc2a5a43ab61c011977cd2a5e719
Ariba Abbai <165388999+Aribaabbaasi@users.noreply.github.com>
2024-03-29 20:28:34 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,9 @@
+let PersonName:string='';
+PersonName = prompt('What is your name?') || '';
+
+if(PersonName !== null && PersonName !== ''){
+    alert(`Hello ${PersonName},would you like to learn some Python today?`);
+}
+else{
+    alert(`Hello ${PersonName}, would you like to learn some Python today?`);
+}
